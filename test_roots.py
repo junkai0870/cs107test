c169ded3ddd57e2453447a95c9d1991e1cc3080a
junkai_ong <junkai_ong@g.harvard.edu>
2020-10-26 14:39:58 +0800

    commit roots.py and test_roots.py

---
+++ test_roots.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 11:09:13 2020
+
+@author: junkaiong
+"""
+
+# taken from lecture
+
+import pytest
+import roots
+
+
+def test_quadroots_result():
+    assert roots.quad_roots(1.0, 1.0, -12.0) == ((3+0j), (-4+0j))
+
+
+def test_quadroots_types():
+    with pytest.raises(TypeError):
+        roots.quad_roots("", "green", "hi")
+
+
+def test_quadroots_zerocoeff():
+    with pytest.raises(ValueError):
+        roots.quad_roots(a=0.0)
